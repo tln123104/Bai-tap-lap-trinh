48b1b827fc6fd1c118425bb6633bfb8f1979e255
tln123104 <lengocthanh_t67@hus.edu.vn>
2025-11-01 10:37:06 +0700

    Refactor comments and maintain code functionality
    Updated comments and code structure for clarity.

--- Chương 6/GIAOTRINH_LAPTRINH_C_V1-CHUONG6/Bai 6.13.cpp
+++ Chương 6/GIAOTRINH_LAPTRINH_C_V1-CHUONG6/Bai 6.13.cpp
@@ -1,5 +1,15 @@
-/*
--	Sử dụng một mảng a gồm 105 phần tử để lưu các phần tử của dãy A.
+/* Dãy số a được xây dựng như sau:
+A[0] = 0
+A[1] = 1
+A[2i] = A[i]
+A[2i + 1] = A[i] + A[i + 1]
+Yêu cầu: Viết chương trình tìm số lớn nhất thuộc đoạn [1, N] trong dãy A,
+với N≤ 10^5
+Việc kiểm tra được thực hiện với T test (T ≤ 10^3). Kết quả in ra T dòng tương
+ứng với giá trị lớn nhất của T đoạn.
+Ví dụ: */
+
+/*-	Sử dụng một mảng a gồm 105 phần tử để lưu các phần tử của dãy A.
 -	Sử dụng một mảng f gồm 105 phần tử,
     trong đó phần tử thứ i của mảng f lưu trữ
     các giá trị lớn nhất của các phần tử của mảng a
@@ -38,3 +48,4 @@ int main()
     }
     return 0;
 }
+
